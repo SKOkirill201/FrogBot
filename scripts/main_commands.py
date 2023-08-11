2b1f13149ed0c94761e440d84cad23393a994a3c
SKOkirill201 <skokirill11@gmail.com>
2023-08-11 23:03:10 +0500

    0.0.7 2
    Добавлена существующая в 0.0.6.1 команда использования аптечки с некоторыми изменениями из-за новой системы здоровья.

--- scripts/main_commands.py
+++ scripts/main_commands.py
@@ -39,3 +39,20 @@ async def main(bot: Bot, message: types.Message, users_: dict):
         await bot.send_message(message.chat.id, 'Вы решили угостить свою лягушку едой, но какой?',
                                reply_markup=markup, 
                                message_thread_id=message.message_thread_id)
+
+    elif str(message.text).lower() in all_commands['use_first_aid_kit']:
+        if users_[str(message.from_user.id)]['user']['first_aid_kits'] > 0:
+            if users_[str(message.from_user.id)]['frog']['is_alive'] != True:
+                users_[str(message.from_user.id)]['user']['first_aid_kits'] -= 1
+                users_[str(message.from_user.id)]['frog']['is_alive'] += 20
+                bot.send_message(message.chat.id, 
+                                 'Вы успешно восстановили здоровье своей лягушке ❤!',
+                                 message_thread_id=message.message_thread_id)
+            else:
+                bot.send_message(message.chat.id, 
+                                 'Ваша лягушка в полном порядке ❤',
+                                 message_thread_id=message.message_thread_id)
+        else:
+            bot.send_message(message.chat.id, 
+                             'У вас нету аптечек!',
+                             message_thread_id=message.message_thread_id)
